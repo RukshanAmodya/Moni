089c60091b8791e84ea49367714067c728c82595
Rukshan Amodya <nmr.amodya@gmail.com>
2026-07-16 22:51:32 +0530

    Resolve Transaction class conflict with cloud_firestore by hiding it from imports

--- lib/providers/finance_provider.dart
+++ lib/providers/finance_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:crypto/crypto.dart';
 import 'dart:convert';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart' hide Transaction;
 import '../models/finance_models.dart';
 import '../services/storage_service.dart';
 
